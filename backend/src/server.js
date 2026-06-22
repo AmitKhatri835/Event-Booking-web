0992e5f024f608d0f6a4e78462f38fe3ec473169
AmitKhatri835 <khatriamit.835@gmail.com>
2026-06-22 16:16:10 +0530

    backend

--- backend/src/server.js
+++ backend/src/server.js
@@ -22,7 +22,21 @@ const app = express();
 
 app.use(express.json());
 
-app.use(cors());
+app.use(
+  cors({
+    origin: function (origin, callback) {
+      if (!origin || allowedOrigins.indexOf(origin) !== -1) {
+        callback(null, true);
+      } else {
+        callback(new Error("Not allowed by CORS"));
+      }
+    },
+    credentials: true,
+  }),
+);
+const allowedOrigins = process.env.ALLOWED_ORIGINS?.split(",") || [
+  "http://localhost:3001",
+];
 
 app.use("/api/auth", authRoutes);
 
